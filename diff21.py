bea1c4110fea46b6c834f9eb2ff403ed86ea8062
SamH <sammihua2010@gmail.com>
2016-12-07 06:17:23 +1100

    Update diff21.py

--- diff21.py
+++ diff21.py
@@ -1,5 +1,12 @@
+#Function Definition
 def diff21(n):
   difference = 21 - n;
   if n > 21:
-    return abs(difference)*2;
+      return abs(difference)*2;
   return abs(difference);
+
+arg1 = 19
+
+#Function Call    
+print ("What is the difference of ", arg1, " and 21?")
+print (diff21(arg1))
