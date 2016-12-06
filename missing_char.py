d7fc12b7576526798a9caced8a3b6eaee7ee51bf
SamH <sammihua2010@gmail.com>
2016-12-07 06:41:56 +1100

    Update missing_char.py

--- missing_char.py
+++ missing_char.py
@@ -1,4 +1,11 @@
+#Function Definition
 def missing_char(str, n):
   front = str[:n]
   back = str[n+1:]
   return front + back
+
+arg1 = 'TROLOLOLOLOLOL'
+arg2 = 5
+
+#Function Call    
+print (missing_char(arg1, arg2))
