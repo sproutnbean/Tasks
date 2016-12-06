61d14601b5bc26f5d34ad13693e5ba98c8ddab28
SamH <sammihua2010@gmail.com>
2016-12-07 06:44:38 +1100

    Update front_back.py

--- front_back.py
+++ front_back.py
@@ -1,6 +1,13 @@
+#Function Definition
 def front_back(str):
   if len(str) <= 1:
     return str;
   
   mid = str[1:-1];
   return str[-1] + mid + str[0];
+
+
+arg1 = 'TROLOLOLOLOLOL'
+
+#Function Call    
+print (front_back(arg1))
