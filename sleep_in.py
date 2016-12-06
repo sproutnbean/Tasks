956d90987a27cabf12bb522df266acb4aa372d61
SamH <sammihua2010@gmail.com>
2016-12-07 05:48:11 +1100

    Update sleep_in.py

--- sleep_in.py
+++ sleep_in.py
@@ -1,7 +1,28 @@
+import sys
+
 #Function Definition
 def sleep_in(weekday, vacation):
-  if not weekday or vacation:
-    return True;
-  else: 
-    return False;
+    if (weekday is False) or (vacation is True) :
+        return True
+    else:
+        return False
 
+#Convert String to Boolean 
+arg1 = False
+arg2 = False
+
+#Running Python in IDLE, disconnect this line if neccessary
+sys.argv = ['sleep_in.py', 'True', 'True']
+        
+if ((sys.argv[1]) == 'True'):
+    arg1 = True
+else:
+    arg1 = False
+    
+if ((sys.argv[2]) == 'True'):
+    arg2 = True
+else:
+    arg2 = False
+
+#Function Call    
+print (sleep_in(arg1,arg2))
