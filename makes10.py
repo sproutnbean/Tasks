23d1a5ece6b8098876fe17b031e941b32f06b1c4
SamH <sammihua2010@gmail.com>
2016-12-07 06:25:09 +1100

    Update makes10.py

--- makes10.py
+++ makes10.py
@@ -1,5 +1,16 @@
+import sys
+
+#Function Definition
 def makes10(a, b):
   if a == 10 or b == 10 or a+b == 10:
     return True;
   else:
     return False;
+
+#Input
+arg1 = 10
+arg2 = 10
+
+#Function Call    
+print ("Make 10? " )
+print (makes10(arg1,arg2))
