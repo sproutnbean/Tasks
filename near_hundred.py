c5f7af6eefacdb711d316a1edd4420fe740c8e7f
SamH <sammihua2010@gmail.com>
2016-12-07 06:27:10 +1100

    Update near_hundred.py

--- near_hundred.py
+++ near_hundred.py
@@ -1,7 +1,15 @@
+#Function Definition
 def near_hundred(n):
   dif1 = 100 - n;
   dif2 = 200 - n;
+  
   if abs(dif1) <= 10 or abs(dif2) <= 10:
     return True;
   else:
     return False;
+
+arg1 = 19
+
+#Function Call    
+print ("Is", arg1, "near 100?")
+print (near_hundred(arg1))
