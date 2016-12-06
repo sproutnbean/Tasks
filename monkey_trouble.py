732149e4c7f578749fa40a6c84b9b22c3c9b93ec
SamH <sammihua2010@gmail.com>
2016-12-07 06:01:23 +1100

    Update monkey_trouble.py

--- monkey_trouble.py
+++ monkey_trouble.py
@@ -1,5 +1,29 @@
-def monkey_trouble(a_smile, b_smile):
-  if (a_smile and b_smile) or (not a_smile and not b_smile):
-    return True;
-  else:
-    return False;
+import sys
+
+#Function Definition
+def monkey_trouble (a_smile, b_smile):
+    if (a_smile and b_smile) or (not a_smile and not b_smile):
+        return True;
+    else:
+        return False;
+
+#Convert String to Boolean 
+arg1 = False
+arg2 = False
+
+#Running Python in IDLE, disconnect this line if neccessary
+sys.argv = ['monkey_trouble.py', 'True', 'True']
+        
+if ((sys.argv[1]) == 'True'):
+    arg1 = True
+else:
+    arg1 = False
+    
+if ((sys.argv[2]) == 'True'):
+    arg2 = True
+else:
+    arg2 = False
+
+#Function Call    
+print ("Got a Monkey Trouble? " )
+print (monkey_trouble(arg1,arg2))
