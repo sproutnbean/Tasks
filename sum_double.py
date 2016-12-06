fcc6557e87edf63c92c9ae8fd7f6630a304f1d95
SamH <sammihua2010@gmail.com>
2016-12-07 06:14:21 +1100

    Update sum_double.py

--- sum_double.py
+++ sum_double.py
@@ -1,5 +1,13 @@
-def sum_double(a, b):
-  if a != b:
-    return a + b;
-  else:
-    return 4*a
+#Function Definition
+def sum_double (a, b):
+    if a != b:
+        return a + b;
+    else:
+        return 4*a
+
+arg1 = 1
+arg2 = 4
+
+#Function Call    
+print ("What is the sum double of ", arg1, " and ", arg2, " ?")
+print (sum_double(arg1 ,arg2))
