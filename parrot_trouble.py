c2b084d49a0dc3bca5f5a0d37c50f2ac3cbef10a
SamH <sammihua2010@gmail.com>
2016-12-07 06:21:22 +1100

    Update parrot_trouble.py

--- parrot_trouble.py
+++ parrot_trouble.py
@@ -1,5 +1,24 @@
-def parrot_trouble(talking, hour):
+import sys
+
+#Function Definition
+def parrot_trouble (talking, hour):
   if (talking and (hour < 7 or hour > 20)):
     return True;
   else:
     return False;
+
+#Running Python in IDLE, disconnect this line if neccessary
+sys.argv = ['parrot_trouble.py', 'True', 'True']
+
+#Convert String to Boolean 
+arg1 = False
+arg2 = 9
+
+if ((sys.argv[1]) == 'True'):
+    arg1 = True
+else:
+    arg1 = False
+
+#Function Call    
+print ("Got Parrot Trouble? " )
+print (parrot_trouble(arg1,arg2))
